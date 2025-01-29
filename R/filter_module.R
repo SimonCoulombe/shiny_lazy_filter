d792ca723ea587b3d35ae84ab6de08873cfc5446
Simon Coulombe <SimonCoulombe@protonmail.com>
2025-01-29 20:01:44 +0000

    works?

--- R/filter_module.R
+++ R/filter_module.R
@@ -18,56 +18,48 @@ filterModuleUI <- function(id) {
   )
 }
 
-filterModule <- function(input, output, session, filter_state) {
-  ns <- session$ns
-
-  # Initialize the filter state if it doesn't exist
-  if (is.null(filter_state$column)) {
-    filter_state$column <- NULL
-    filter_state$type <- NULL
-    filter_state$range <- NULL
-    filter_state$categories <- NULL
-  }
-
-  # Update the column selection
-  observe({
-    updateSelectInput(session, "column", selected = filter_state$column)
-  })
+filterModule <- function(input, output, session) {
+  message("Initializing filter module")
+  filter_state <- reactiveVal(NULL)
 
   output$filter_ui <- renderUI({
+    ns <- session$ns
     req(input$column)
 
-    if (get_column_type(input$column) == "numeric") {
+    if(get_column_type(input$column) == "numeric") {
       range <- get_range(input$column)
       sliderInput(ns("range"), "Select Range",
                   min = range[1], max = range[2],
-                  value = filter_state$range %||% range,
-                  step = (range[2] - range[1]) / 100)
+                  value = range,
+                  step = (range[2] - range[1])/100)
     } else {
       choices <- get_distinct_values(input$column)
       selectizeInput(ns("categories"), "Select Categories",
                      choices = choices,
                      multiple = TRUE,
-                     selected = filter_state$categories %||% choices,
+                     selected = choices,
                      options = list(plugins = list('remove_button')))
     }
   })
 
-  # Update the filter state when inputs change
   observe({
     req(input$column)
     col_type <- get_column_type(input$column)
 
-    if (col_type == "numeric") {
+    if(col_type == "numeric") {
       req(input$range)
-      filter_state$column <- input$column
-      filter_state$type <- "numeric"
-      filter_state$range <- input$range
+      filter_state(list(
+        column = input$column,
+        type = "numeric",
+        range = input$range
+      ))
     } else {
       req(input$categories)
-      filter_state$column <- input$column
-      filter_state$type <- "categorical"
-      filter_state$categories <- input$categories
+      filter_state(list(
+        column = input$column,
+        type = "categorical",
+        categories = input$categories
+      ))
     }
   })
 
